0506bc2d66722330c76182d261f4aed9fd726409
AmandeepS03 <amandeep04singh@gmail.com>
2023-07-23 10:44:51 +0200

    fixed issues

--- PackAndTravel/src/main/java/control/VisualizzaProdottiServlet.java
+++ PackAndTravel/src/main/java/control/VisualizzaProdottiServlet.java
@@ -23,7 +23,7 @@ import model.ProdottoDAO;
 @WebServlet("/VisualizzaProdottiServlet")
 public class VisualizzaProdottiServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final Logger logger = Logger.getLogger(LoginServlet.class.getName());
+	private static Logger logger = Logger.getAnonymousLogger();
 
     
     public VisualizzaProdottiServlet() {
